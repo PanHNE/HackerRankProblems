7537f02ff73449e97f6a932c6c604967d3c2a4fb
Adrian Domin <adriandominx@gmail.com>
2023-08-10 16:33:21 +0200

    Created solution for problem and checked solution manually

---
+++ CompareTheTriplets/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareTheTriplets
+{
+    internal class Result
+    {
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            var aPoints = 0;
+            var bPoints = 0;
+
+            for (int i = 0; i < a.Count(); i++)
+            {
+                if (a[i] > b[i]) aPoints++;
+                else if (a[i] < b[i]) bPoints++;
+            }
+
+            return new List<int> { aPoints, bPoints };
+
+        }
+    }
+}
